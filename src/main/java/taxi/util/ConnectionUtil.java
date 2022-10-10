8facf754fc65796d9eaf3fd1dc45f1f62f7192f6
Serhii Buria <sergeiburya@gmail.com>
2022-10-11 00:06:43 +0300

    added project file

---
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package taxi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://sql11.freemysqlhosting.net/sql11523249";
+    private static final String USERNAME = "sql11523249";
+    private static final String PASSWORD = "ZJZ2du1lac";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
+    }
+}
